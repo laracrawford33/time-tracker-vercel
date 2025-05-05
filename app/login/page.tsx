6e1d66ee3092d48842b54c80f81836be67863f0d
LaraCrawford01 <lara@zoomfibre.co.za>
2025-05-05 14:43:05 +0200

    added github login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,6 @@
 import { supabase } from "@/lib/supabaseClients";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import router from "next/router";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -15,20 +14,20 @@ export default function LoginPage() {
     });
   }, []);
 
-  //   const handleLogin = async () => {
-  //     await supabase.auth.signInWithOAuth({
-  //       provider: "google",
-  //     });
-  //   };
+  const handleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+    });
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Login</h1>
       <button
-        onClick={() => router.push("/")}
+        onClick={handleLogin}
         className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 transition duration-200"
       >
-        Login with Google
+        Login with github
       </button>
     </div>
   );
